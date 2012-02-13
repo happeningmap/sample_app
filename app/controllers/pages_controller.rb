90a068201a5529d493bd08a60af34499e15ad4ba
Ehsan Afkhami <ehsan@happeningmap.com>
2012-02-13 16:14:01 -0500

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,16 @@
 class PagesController < ApplicationController
   def home
+    @title = "HOME"
   end
 
   def contact
+    @title = "CONTACT"
+    
   end
+  
+  def about
+    @title = "ABOUT"
+    
+  end
+  
 end
--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,14 @@ module SampleApp
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+    
+    
+    if Rails.env.test?
+      initializer :after => :initialize_dependency_mechanism do
+        ActiveSupport::Dependencies.mechanism = :load
+      end
+    end
+    
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
